37993f7300868218e3eb32e5b6c14eb12c1419dc
Elżbieta Krzeczek <krzeczekelzbieta@gmail.com>
2018-11-08 11:31:52 +0100

    Comments

--- src/component/counters.jsx
+++ src/component/counters.jsx
@@ -6,12 +6,18 @@ import Counter from "./counter";
 class Counters extends Component {
   render() {
     console.log("Counters-rendered");
+    //creating variables depenting on this.props,
+    // so that to use them insted of e.g this.props.onIncrement
     const { onRest, counter, onDelete, onIncrement } = this.props;
     return (
       <div>
+        {/* calling onReset function from App.js based on handlereset (creating event) onclick action */}
         <button onClick={onRest} className="btn primary sm m-2">
           Reset
         </button>
+
+        {/* creating the list of components (counter) and specifying id 
+        and functions on the functions to call" onDelete and onIncrement */}
         {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
@@ -27,3 +33,4 @@ class Counters extends Component {
 }
 
 export default Counters;
+
